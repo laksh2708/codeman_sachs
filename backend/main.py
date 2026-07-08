b9f62620cb730fa212efc95775ff38abd9173520
lakshman2708 <lakshmanshrinath27@gmail.com>
2026-07-08 12:43:50 +0530

    phase 2 backend changes code_blocks

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,27 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from routes.auth import router as auth_router
+from routes.code import router as code_router
 
+origins = [
+    "http://localhost:5173",  # Vite development server
+]
 
 app = FastAPI(
     title="CodeMan Sachs Backend",
     version="1.0.0"
 )
-
-
 app.include_router(auth_router)
+app.include_router(code_router)
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,      # Or ["*"] for development only
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
